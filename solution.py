39e50419b85aaa7f5b67d572d2ea1e3819c5ff36
taf9865 <taf9865@nyu.edu>
2022-02-05 12:09:58 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,15 +9,15 @@ def welcome_assignment_answers(question):
 
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "mtls"
-        # elif question == "Are encoding and encryption the same? - Yes/No":
-        #     answer = "No"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+         answer = "No"
         # elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         #answer = "No"
     #elif question == "Is it possible to decode a message without a key? - Yes/No":
         #answer = "Yes"
     #elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         #answer = "No"
-    #elif question == "What is the MD5 hashing value to the following message: NYU Computer Networking":
+    #elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking'":
         #answer = "42b76fe51778764973077a5a94056724"
     #elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
        # answer = "No"
@@ -37,8 +37,8 @@ if __name__ == "__main__":
     # use this space to debug and verify that the program works
     debug_question1 = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
     print(welcome_assignment_answers(debug_question1))
-    #debug_question2 = "Are encoding and encryption the same? - Yes/No"
-    #print(welcome_assignment_answers(debug_question2))
+    debug_question2 = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question2))
     #debug_question3 = "Is it possible to decrypt a message without a key? - Yes/No"
     #print(welcome_assignment_answers(debug_question3))
     #debug_question4 = "Is it possible to decode a message without a key? - Yes/No"
