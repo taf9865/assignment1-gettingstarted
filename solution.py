bb35fc90ca289c3f2ce9f2fa1bb201cfab2915fe
taf9865 <taf9865@nyu.edu>
2022-02-05 05:55:07 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,7 @@ def welcome_assignment_answers(question):
     # Students do not have to follow the skeleton for this assignment.
     # Another way to implement is using a "case" statements similar to C.
 
-    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted " \
-                   "by a TA?":
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "mtls"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
@@ -36,8 +35,7 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     # use this space to debug and verify that the program works
-    debug_question1 = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel " \
-                      "posted by a TA? "
+    debug_question1 = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
     print(welcome_assignment_answers(debug_question1))
     debug_question2 = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question2))
@@ -57,12 +55,12 @@ if __name__ == "__main__":
     print(welcome_assignment_answers(debug_question9))
 
 ###Questions:
-###"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
-###"Are encoding and encryption the same? - Yes/No"
-###"Is it possible to decrypt a message without a key? - Yes/No"
-###"Is it possible to decode a message without a key? - Yes/No"
-###"Is a hashed message supposed to be un-hashed? - Yes/No"
+###"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?" -1
+###"Are encoding and encryption the same? - Yes/No" -2
+###"Is it possible to decrypt a message without a key? - Yes/No" - 3
+###"Is it possible to decode a message without a key? - Yes/No" - 4
+###"Is a hashed message supposed to be un-hashed? - Yes/No" - 5
 ###"What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
-###"Is MD5 a secured hashing algorithm? - Yes/No"
-###"What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
-###"What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
+###"Is MD5 a secured hashing algorithm? - Yes/No" -7
+###"What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number" - 8
+###"What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number" - 9
